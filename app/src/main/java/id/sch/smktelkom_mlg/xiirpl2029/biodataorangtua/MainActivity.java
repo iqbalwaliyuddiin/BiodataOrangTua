faab3aa7c89f31685fbc4ab7560cb2a4afb957c9
iqbalwlyddn <muhammadiqbalwaliyuddiin@gmail.com>
2016-10-15 12:14:04 +0700

    Initial Commit

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl2029/biodataorangtua/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl2029/biodataorangtua/MainActivity.java
@@ -2,12 +2,30 @@ package id.sch.smktelkom_mlg.xiirpl2029.biodataorangtua;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNama;
+    Button btOk;
+    TextView tvHasil;
+    RadioGroup rgStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        btOk = (Button) findViewById(R.id.button);
+        tvHasil = (TextView) findViewById(R.id.tvHasil);
+        rgStatus = (RadioGroup) findViewById(R.id.rgStatus);
+
+
+
     }
 }
